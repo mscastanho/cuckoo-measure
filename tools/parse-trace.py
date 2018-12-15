977eecedc62c595c7f0f49cebebc8da85ab95b69
Matheus Castanho <mscastanho@gmail.com>
2018-12-15 16:32:49 -0200

    Improved script to output filtered pcap trace file

--- tools/parse-trace.py
+++ tools/parse-trace.py
@@ -59,17 +59,22 @@ def main(argv):
     parser.add_argument('-c', '--csv', action='store_const', const=True,
         default=False,help='Output results as csv file')
 
+    parser.add_argument('-p', '--print', action='store_const', const=True,
+        default=False,help='Output results to stdout')
+
     args = vars(parser.parse_args())
 
-    output_csv = args['csv'] 
+    output_csv = args['csv']
+    output_stdout = args['print'] 
     filename = args['file']
 
     flows = {}
 
     cnt = 0
     start_time = time.time()
-    PKT_CNT_LIMIT = 5_000_000
+    PKT_CNT_LIMIT = 10_000
     FLOW_MIN_PKTS = 100
+    out_file = "selected_flows_%s_%s.pcap" % (PKT_CNT_LIMIT,FLOW_MIN_PKTS)
     for pkt in PcapReader(filename):
         # print("ipsrc: %s\tipdst: %s\tproto: %s" % (pkt[IP].src,pkt[IP].src,pkt[IP].proto))
         try:
@@ -92,25 +97,47 @@ def main(argv):
         if cnt > PKT_CNT_LIMIT:
             break
 
+    selected = {}
     if output_csv:
-        with open("trace_summary_%s_%s.csv" % (PKT_CNT_LIMIT,FLOW_MIN_PKTS),"w") as csvfile:
-            colnames = ['IP src','IP dst','Protocol','Src port','Dst port','Packet count','Average interarrival']
-            writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
-            # writer.writeheader()
-            writer.writerow(colnames)
+        csvfile = open("trace_summary_%s_%s.csv" % (PKT_CNT_LIMIT,FLOW_MIN_PKTS),"w")
+        colnames = ['IP src','IP dst','Protocol','Src port','Dst port','Packet count','Average interarrival']
+        writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+        # writer.writeheader()
+        writer.writerow(colnames)
 
-            for f in flows:
-                tk = flows[f]
-                if tk.count > FLOW_MIN_PKTS:
-                    writer.writerow(list(f)+[tk.count,"%.6f" % tk.avg_interarrival()])
-    else:
-        for f in flows:
-            tk = flows[f]
-            if tk.count > FLOW_MIN_PKTS:
+    for f in flows:
+        tk = flows[f]
+        if tk.count > FLOW_MIN_PKTS:
+            selected[f] = tk.avg_interarrival()
+            if output_csv:
+                writer.writerow(list(f)+[tk.count,"%.6f" % tk.avg_interarrival()])
+            if output_stdout:
                 print("%s\t:\t%s" % (f,flows[f]))
+
+    cntsel = 0
+    cnt = 0
+    # Create new pcap only with packets from selected flows
+    with PcapWriter(out_file) as pw:
+        for pkt in PcapReader(filename):
+            try:
+                t = get_5tuple(pkt)
+            except Exception as e:
+                # print("Error: %s" % str(e))
+                # print(pkt.summary())
+                continue
+            
+            if t in selected:
+                cntsel += 1
+                pw.write(pkt)
+            
+            cnt += 1
+            if cnt > PKT_CNT_LIMIT:
+                break
                 
     time_elapsed = time.time() - start_time
-    print("\n\n# of packets: %d\n# of flows: %d\nTime elapsed: %f" % (cnt,len(flows),time_elapsed))
+    print("\n\n# of pkts: %d\n# of flows: %d" % (cnt,len(flows)))
+    print("# of selected pkts: %d\n# of selected flows: %d" % (cntsel,len(selected)))
+    print("Time elapsed: %f" % time_elapsed)
 
 if __name__ == "__main__":
     main(sys.argv)
